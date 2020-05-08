b460b7f8c00e9797be2b054184f11c29312da40e
Sabastian <sabastianp@hotmail.ca>
2020-05-08 00:00:01 -0700

    added: clears source and dest folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ const gulp = require("gulp");
 const gulpExec = require("gulp-exec");
 const exec = require('child_process').exec;
 
-let UnityPath = "";
-let ProjectFolderName = "project-src";
+let UnityVersion = "2019.3.12f1";
+
+let ProjectSourceFolderName = "project-src";
+let ProjectDestFolderName = "project-src";
+
 let GitUrl = "https://github.com/vfs-sct/Afloat";
 let TargetBranch = "develop";
+let UnityPath = `C:\\Program Files\\Unity Editors\\${UnityVersion}\\Editor\\Unity.exe`;
+
+
 
 function runCmdNoError (done, cmd)
 {
@@ -27,15 +33,23 @@ function runCmd (done, cmd)
 }
 
 
-gulp.task("clear", done => {
-    runCmdNoError(done, `rd /s /q "%cd%\\${ProjectFolderName}"`)
-})
+
+gulp.task("clear-src", done => {
+    runCmdNoError(done, `rd /s /q "%cd%\\${ProjectSourceFolderName}"`)
+});
+gulp.task("clear-dest", done => {
+    runCmdNoError(done, `rd /s /q "%cd%\\${ProjectDestFolderName}"`)
+});
+
+gulp.task("clear", gulp.parallel(["clear-src", "clear-dest"]));
 
 gulp.task("pull", done => {
-    runCmd(done, `git clone -b ${TargetBranch} ${GitUrl} "${ProjectFolderName}"`)
+    runCmd(done, `git clone -b ${TargetBranch} ${GitUrl} "${ProjectSourceFolderName}"`)
 });
 
 gulp.task("unity-build", done => {
+    // runCmd(done, "mkdir $")
+    runCmd(done, `"${UnityPath}" -quit -batchmode -logFile stdout.log -projectPath "%cd%\\${ProjectSourceFolderName}" -buildWindowsPlayer "%cd$\\${ProjectDestFolderName}"`)
 });
 
 
