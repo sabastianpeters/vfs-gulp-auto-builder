4127cfcbd35df7644f07da40c7d0bb34609affd7
Sabastian <sabastianp@hotmail.ca>
2020-05-07 23:33:22 -0700

    added: now clones a given repo from git

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 
-
-const gulp = require("gulp")
-const gulpExec = require("gulp-exec")
-const exec = require('child_process').exec
+const gulp = require("gulp");
+const gulpExec = require("gulp-exec");
+const exec = require('child_process').exec;
 
 let UnityPath = "";
 let ProjectFolderName = "project-src";
+let GitUrl = "https://github.com/sabastianpeters/VFS-Portfolio";
+
+
+function runCmdNoError (done, cmd)
+{
+    exec(cmd, function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        done();
+    })
+}
 
 function runCmd (done, cmd)
 {
@@ -17,9 +27,13 @@ function runCmd (done, cmd)
 }
 
 
+
 gulp.task("pull", done => {
-    runCmd(done, `mkdir ${ProjectFolderName}`)
+    runCmd(done, `git clone ${GitUrl} "${ProjectFolderName}"`)
+});
+
+gulp.task("unity-build", done => {
 });
 
 
-gulp.task("default", gulp.series("pull"));
+gulp.task("default", gulp.series("pull", "unity-build"));
