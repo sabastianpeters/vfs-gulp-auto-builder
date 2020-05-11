c4e055613ceb940c78882b831b495629095480f7
Sabastian <sabastianp@hotmail.ca>
2020-05-10 21:02:05 -0700

    added: uses 7z to zip windows build sucessfully

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,17 @@ const GAME_NAME = "Project Afloat"
 
 const PROJECT_SOURCE_PATH = "project-src";
 const PROJECT_DEST_PATH = `project-dest`;
+
+
+
+
 const WINDOWS_EXE_PATH = `${PROJECT_DEST_PATH}\\Windows\\${GAME_NAME}.exe`;
 const OSX_EXE_PATH = `${PROJECT_DEST_PATH}\\Mac\\${GAME_NAME}.exe`;
 const LINUX_EXE_PATH = `${PROJECT_DEST_PATH}\\Linux\\${GAME_NAME}.exe`;
+
+const WINDOWS_BUILD_ZIP_PATH = `${PROJECT_DEST_PATH}\\Compressed\\Windows\\${GAME_NAME}.zip`;
+const OSX_BUILD_ZIP_PATH = `${PROJECT_DEST_PATH}\\Compressed\\Mac\\${GAME_NAME}.zip`;
+const LINUX_BUILD_ZIP_PATH = `${PROJECT_DEST_PATH}\\Compressed\\Linux\\${GAME_NAME}.zip`;
 
 const GitUrl = "https://github.com/vfs-sct/Afloat";
 const TargetBranch = "develop";
@@ -63,6 +71,7 @@ gulp.task("pull", done => {
 
 
 
+
 // ## BUILD TASKS ##
 
 
@@ -89,6 +98,36 @@ gulp.task("rebuild-unreal", gulp.series("clear-dest", "build-unreal"));
 
 
 
+// ## COMPRESS TASKS ##
+
+const bin7z = require("7zip-bin").path7za;
+const node7z = require("node-7z")
+const path = require("path")
+
+gulp.task("compress-builds", (done) => {
+
+    const myStream = node7z.add(WINDOWS_BUILD_ZIP_PATH, path.join(__dirname, path.dirname(WINDOWS_EXE_PATH), "\\*"), {
+        recursive: false, /// this adds things from subfolders
+        $progress: true, /// sends progress events
+        $bin: bin7z, /// reference to 7z
+    })
+    
+
+    myStream.on('progress', function (progress) {
+        console.log(`zip progress: ${progress.percent}`); /// { percent: 67, fileCount: 5, file: undefinded }
+    })
+
+
+    myStream.on('end', function () {    
+        console.log(`zip complete`);
+        done();
+    })
+
+});
+
+
+
+
 
 
 // ## UPLOAD TASKS ##
@@ -104,7 +143,15 @@ gulp.task("upload", async (done) => {
     let credentials = await gAuth.loadCredentials();
     await gAuth.authorize(credentials.installed);
 
-    await gDriveTools.createFileStructure(GAME_NAME)
+    // await gDriveTools.createFileStructure(GAME_NAME) /// use this to create the file structure
+    
+    const GOOGLE_FILE_ID_LIST = {
+        WINDOWS: "a",
+        OSX: "a",
+        LINUX: "a"
+    }
+
+
 
     done();
 })
@@ -118,4 +165,11 @@ gulp.task("upload", async (done) => {
 
 // gulp.task("default", gulp.series("clear", "pull", "build-unity"));
 // gulp.task("default", gulp.series("rebuild"));
-gulp.task("default", gulp.series("upload"));
+gulp.task("default", gulp.series("compress-builds"));
+
+
+
+
+// cli: gulp "full-unity-build"
+// cli: gulp "full-unreal-build"
+// gulp.task("full-unreal-build", gulp.series("clear", "pull", "build-unreal"));
